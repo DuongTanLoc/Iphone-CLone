f9dd3d2e9d0472a24dec6b2bba135f289205308d
Loc <lok1352004@gmail.com>
2024-09-27 07:02:02 +0700

    modified:   src/components/VideoCarousel.jsx

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -51,14 +51,18 @@ const VideoCarousel = () => {
   const handleLoadedMetaData = (i, e) => setLoadedData((pre)=>[...pre, e])
 
   useEffect(()=> {
-    const currentProgress = 0;
+    let currentProgress = 0;
     let span = videoSpanRef.current;
 
     if(span[videoId]){
       //animate the progress of the video
       let anim  = gsap.to(span[videoId], {
         onUpdate : () =>{
-
+          const progress = Math.ceil(anim.progress()*100);
+          if(progress != currentProgress){
+            currentProgress = progress;
+            gsap.to(videoDivRef.current[videoId], {width : window.innerWidth < 760})
+          }
         },
         onComplete : () =>{
 
